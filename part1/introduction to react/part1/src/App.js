b1be846f4012d60e3d5752c3d8dc7319d0a5e8e9
Zheyuan Xu <u6964086@anu.edu.au>
2022-03-11 01:38:12 +1100

    Component state, event handlers

--- part1/introduction to react/part1/src/App.js
+++ part1/introduction to react/part1/src/App.js
@@ -1,33 +1,46 @@
-import React from 'react';
+// Stateful component
+//import React from "react";
+// import { useState } from "react";
 
-const Hello = (props) => {
-  return (
-    <div>
-      Hello {props.name}, you are {props.age} years old
-    </div>
-  )
-}
+// const App = () => {
+//   //setCounter modifies the state it causes the component to re-render
+//   const [counter, setCounter] = useState(0); //assign the items to the variables counter and setCounter by using the destructuring assignment syntax shown earlier
+//   setTimeout(() => setCounter(counter + 1), 1000); //passes it two parameters: a function to increment the counter state and a timeout of one second
+//   // The function passed as the first parameter to the setTimeout function is invoked one second after calling the setTimeout function
 
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-    </div>
-  )
-}
+//   console.log("rendering...", counter);
+//   return <div>{counter}</div>;
+// };
+
+// export default App;
+
+/// Event handling
+import React from "react";
+import { useState } from "react";
+
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [counter, setCounter] = useState(0);
 
+  // <button onClick={setCounter(counter + 1)}>
+  //it executes the function call setCounter(0+1), and changes the value of the component's state to 1. This will cause the component to be re-rendered,
+  //React will execute the setCounter function call again, and the state will change leading to another rerender...
+
+  //() => setCounter(counter + 1) The setCounter function is called only when a user clicks the button.
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Footer />
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />{" "}
+      <Button onClick={setToZero} text="zero" />{" "}
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
-  )
-}
-
-
-export default App
+  );
+};
+export default App;
--- part1/introduction to react/part1/src/index.js
+++ part1/introduction to react/part1/src/index.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
-ReactDOM.render(<App />, document.getElementById('root')) //renders its contents into the div-element, defined in the file public/index.html, having the id value 'root'.
+ReactDOM.render(<App />, document.getElementById("root"));
