3abe801c9aafcfc969a57ef819aabbde91801f69
Zheyuan Xu <u6964086@anu.edu.au>
2022-03-13 09:57:49 +1100

    to d.debugging

--- part1/introduction to react/part1/src/App.js
+++ part1/introduction to react/part1/src/App.js
@@ -15,31 +15,98 @@
 // export default App;
 
 /// Event handling
+// import React from "react";
+// import { useState } from "react";
+
+// const Display = ({ counter }) => <div>{counter}</div>;
+
+// const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0);
+
+//   // <button onClick={setCounter(counter + 1)}>
+//   //it executes the function call setCounter(0+1), and changes the value of the component's state to 1. This will cause the component to be re-rendered,
+//   //React will execute the setCounter function call again, and the state will change leading to another rerender...
+
+//   //() => setCounter(counter + 1) The setCounter function is called only when a user clicks the button.
+
+//   const increaseByOne = () => setCounter(counter + 1);
+//   const decreaseByOne = () => setCounter(counter - 1);
+//   const setToZero = () => setCounter(0);
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button onClick={increaseByOne} text="plus" />{" "}
+//       <Button onClick={setToZero} text="zero" />{" "}
+//       <Button onClick={decreaseByOne} text="minus" />
+//     </div>
+//   );
+// };
+// export default App;
+
+///part d: Complext state
 import React from "react";
 import { useState } from "react";
 
-const Display = ({ counter }) => <div>{counter}</div>;
+const History = (props) => {
+  /// conditional rendering
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  // <button onClick={setCounter(counter + 1)}>
-  //it executes the function call setCounter(0+1), and changes the value of the component's state to 1. This will cause the component to be re-rendered,
-  //React will execute the setCounter function call again, and the state will change leading to another rerender...
+  // const handleLeftClick = () => {
+  //   setClicks({
+  //     ...clicks, /// creates a new object that has copies of all of the properties of the clicks object
+  //     left: clicks.left + 1,
+  //   });
+  // };
 
-  //() => setCounter(counter + 1) The setCounter function is called only when a user clicks the button.
+  // const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  // const handleRightClick = () => {
+  //   setClicks({
+  //     ...clicks,
+  //     right: clicks.right + 1,
+  //     /// react forbides mutate state directly, since it can result in unexpected side effects. Changing state has to always be done by setting the state to a new object.
+  // // clicks.left++
+  // // setClicks(clicks)
+  //   });
+  // };
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L")); // does not mutate the existing array but rather returns a new copy of the array with the item added to it
+
+    /// the following is not good as the state of React components like allClicks must not be mutated directly
+    // allClicks.push('L')
+    // setAll(allClicks)
+    // setLeft(left + 1)
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
+
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />{" "}
-      <Button onClick={setToZero} text="zero" />{" "}
-      <Button onClick={decreaseByOne} text="minus" />
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 };
