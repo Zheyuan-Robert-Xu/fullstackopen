edfe000ef14b8d426ff3f2acd89aedc445e5f58b
Zheyuan Xu <u6964086@anu.edu.au>
2022-03-20 07:01:39 +1100

    exercise 2.2

--- part2/exercise/src/App.js
+++ part2/exercise/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Header = ({ course }) => <h1>{course}</h1>;
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+const Total = ({ sum }) => (
+  <p>
+    <strong>total of {sum} exercises</strong>
+  </p>
+);
 
 const Part = ({ part }) => (
   <p>
@@ -13,13 +17,14 @@ const Course = ({ course }) => (
   <div>
     <Header course={course.name} />
     <Content parts={course.parts} />
-    {/* <Total
+    <Total
       sum={
         course.parts[0].exercises +
         course.parts[1].exercises +
-        course.parts[2].exercises
+        course.parts[2].exercises +
+        course.parts[3].exercises
       }
-    /> */}
+    />
   </div>
 );
 
@@ -28,6 +33,7 @@ const Content = ({ parts }) => (
     <Part part={parts[0]} />
     <Part part={parts[1]} />
     <Part part={parts[2]} />
+    <Part part={parts[3]} />
   </div>
 );
 
@@ -49,6 +55,11 @@ const App = () => {
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
         id: 3,
       },
     ],
