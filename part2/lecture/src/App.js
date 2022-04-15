06e001d503e3d086910002e90a7c1976101f91fc
Zheyuan Xu <u6964086@anu.edu.au>
2022-04-16 05:46:24 +1000

    add error message

--- part2/lecture/src/App.js
+++ part2/lecture/src/App.js
@@ -3,7 +3,22 @@ import Note from "./components/Note";
 import React from "react";
 import noteService from "./services/notes";
 import Notification from "./components/Notification";
-
+const Footer = () => {
+  // Inline styles
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2022
+      </em>
+    </div>
+  );
+};
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
@@ -84,6 +99,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
