8650eb742582fcd70090a2f349d05590ae70464f
Zheyuan Xu <u6964086@anu.edu.au>
2022-04-15 17:52:44 +1000

    not dynamically change

--- part2/exercise/phonebook/src/components/Persons.js
+++ part2/exercise/phonebook/src/components/Persons.js
@@ -1,14 +1,32 @@
 import React from "react";
 import Person from "./Person";
+import noteService from "../services/notes";
 
 const Persons = ({ persons, newFilter }) => {
+  const handleDelete = (personToDelete) => () => {
+    const id = persons.find((person) => person.id === personToDelete.id).id;
+    console.log(id);
+    // ask user to confirm before delete operations
+    let confirmation = window.confirm(`Delete ${personToDelete.name}?`);
+    if (confirmation) {
+      noteService.deleteNote(id);
+    }
+  };
   return (
     <div>
       {persons.map((person) =>
         newFilter === "" ? (
-          <Person key={person.name} person={person} />
+          <div>
+            <div key={person.id}>
+              {person.name} {person.number}
+              <button onClick={handleDelete(person)}>Delete</button>
+            </div>
+          </div>
         ) : person.name.toLowerCase().includes(newFilter.toLowerCase()) ? (
-          <Person key={person.name} person={person} />
+          <div key={person.id}>
+            {person.name} {person.number}
+            <button onClick={handleDelete(person)}>Delete</button>
+          </div>
         ) : null
       )}
     </div>
--- part2/exercise/phonebook/src/services/notes.js
+++ part2/exercise/phonebook/src/services/notes.js
@@ -22,13 +22,13 @@ const create = (newObject) => {
   return request.then((response) => response.data);
 };
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
+const deleteNote = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
   return request.then((response) => response.data);
 };
 
 export default {
   getAll,
   create,
-  update,
+  deleteNote,
 };
